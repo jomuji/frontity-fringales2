40cc4c0470cf7d5fd35aa981fa8d286231cba6c5
Jose Mujica <jose@netavantage.com>
2020-08-31 22:48:10 -0400

    title change

--- frontity.settings.js
+++ frontity.settings.js
@@ -3,8 +3,8 @@ const settings = {
   "state": {
     "frontity": {
       "url": "https://test.frontity.org",
-      "title": "Test Frontity Blog",
-      "description": "WordPress installation for Frontity development"
+      "title": "Food la bouffe",
+      "description": "Foodlabouffe - Recettes du web"
     }
   },
   "packages": [
